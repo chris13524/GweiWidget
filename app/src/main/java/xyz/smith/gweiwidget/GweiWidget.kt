954ac67ad60093220db3b404e7ce1dc010c59c6d
Chris Smith <chris@smith.xyz>
2024-07-17 00:30:18 -0700

    Add Etherscan API

--- app/src/main/java/xyz/smith/gweiwidget/GweiWidget.kt
+++ app/src/main/java/xyz/smith/gweiwidget/GweiWidget.kt
@@ -5,6 +5,14 @@ import android.appwidget.AppWidgetProvider
 import android.content.Context
 import android.widget.RemoteViews
 
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 /**
  * Implementation of App Widget functionality.
  */
@@ -34,11 +42,65 @@ internal fun updateAppWidget(
     appWidgetManager: AppWidgetManager,
     appWidgetId: Int
 ) {
-    val widgetText = context.getString(R.string.appwidget_text)
-    // Construct the RemoteViews object
-    val views = RemoteViews(context.packageName, R.layout.gwei_widget)
-    views.setTextViewText(R.id.appwidget_text, widgetText)
+    val etherscanApi = RetrofitClient.instance.create(EtherscanApi::class.java)
+    val call = etherscanApi.getGasOracle("gastracker", "gasoracle", apiKey)
 
-    // Instruct the widget manager to update the widget
-    appWidgetManager.updateAppWidget(appWidgetId, views)
+    call.enqueue(object : Callback<GasOracleResponse> {
+        override fun onResponse(call: Call<GasOracleResponse>, response: Response<GasOracleResponse>) {
+            if (response.isSuccessful) {
+                val proposeGasPrice = response.body()?.result?.ProposeGasPrice
+
+                val gwei = proposeGasPrice
+                val widgetText = "${gwei} gwei"
+                // Construct the RemoteViews object
+                val views = RemoteViews(context.packageName, R.layout.gwei_widget)
+                views.setTextViewText(R.id.appwidget_text, widgetText)
+
+                // Instruct the widget manager to update the widget
+                appWidgetManager.updateAppWidget(appWidgetId, views)
+            } else {
+                error(response)
+            }
+        }
+
+        override fun onFailure(call: Call<GasOracleResponse>, t: Throwable) {
+            error(t)
+        }
+    })
+}
+
+data class GasOracleResponse(
+    val status: String,
+    val message: String,
+    val result: Result
+)
+
+data class Result(
+    val LastBlock: String,
+    val SafeGasPrice: String,
+    val ProposeGasPrice: String,
+    val FastGasPrice: String,
+    val suggestBaseFee: String,
+    val gasUsedRatio: String
+)
+
+interface EtherscanApi {
+    @GET("api")
+    fun getGasOracle(
+        @Query("module") module: String,
+        @Query("action") action: String,
+        @Query("apikey") apikey: String
+    ): Call<GasOracleResponse>
+}
+
+
+object RetrofitClient {
+    private const val BASE_URL = "https://api.etherscan.io/"
+
+    val instance: Retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
 }
